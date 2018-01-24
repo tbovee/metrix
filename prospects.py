e219e0dd4acc7d6973848d36442698967e883608
tbovee <tbovee@gmail.com>
2018-01-23 16:27:49 -0800

    On branch master Initial commit Changes to be committed:      new file:   prospects.py

---
+++ prospects.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+# Import modules
+import sys
+
+# Optional modules
+import pandas
+
+def main() :
+  print "Howdy, Pardner!"
+
+
+# CALL MAIN
+
+if __name__ == '__main__' :
+  main()
+
+# --EOF--
