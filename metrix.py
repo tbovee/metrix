b2c4943293296e339cb58bce23ed07cc82272fb9
tbovee <tbovee@gmail.com>
2018-02-02 12:25:04 -0800

    metrix.py

--- metrix.py
+++ metrix.py
@@ -1,57 +1,71 @@
 #! /usr/bin/env python
 
 # Version 0.0
-# DTG 20180129.1950
+# DTG 20180202.1225 Added the data file class and defined the input file objects
+  for the 1st run; next step is to define the output file objects.Q: How do I override
+  a method defined in the class?
+# DTG 20180129.1950 template created
+# See Documentation.txt for an explanation of metrix.py
 
 # Import modules
 import sys
 
 # Optional modules
-import pandas
+import pandas as pd
+import numpy as np
 
-'''
-metrix performs these jobs:
+# File constants
+fsep = "/"
+datapath = "." + fsep + "data" + fsep
 
-	1) imports a specific set of csv tables into pandas dataframes. The tables are
-		downloaded before metrix is invoked. The tables are z.csv and e.csv.
-		a) First, an inner join
-			i) z.csv :: Daily analysis data from Zacks, updated daily in the morning
-				approximately 7,000 records
-			ii) e.csv :: Earnings schedule data from Zacks, updated once weekly 
-				Up to approximately 2,000 records
-			Result: earns01.csv
-		b) Second join, a left outer join:
-			i) zearns.csv (the left, all records retained)
-			ii) w.csv :: A list of symbols having weekly options, updated as needed
-				to capture changes
-			iii) binary.csv  :: A list of brokerage-recognizable symbols corresponding to 
-				binary options on Nadex
-			iv) The combined table is reconstructed with flag fields marking symbols 
-				having weekly options and symbols used to analyze binary options.
-			Result: earns_interim.csv
-		c) A manual stage to allow for download of the brokerage data.
-			i) metrix Writes the full list of symbols in zearns_interim to file syms.csv
-			ii) metrix pauses for manual creation of the market-data table m.csv
-			iii) metrix resumed when notified by the operator.		
-		c) Third join, a left outer join:
-			i) earns_interim.csv (the left, all records retained as an error check)			
-			ii) m.csv :: Current market information :: updated as needed, from 
-				brokerage ThinkOrSwim
-			Result: Earns mmdd-mmdd, where mmdd are the start and end dates of the
-				period covered, derived programmatically from the minimum and maximum
-				earnings announcements dates in the set. For example, the file for the
-				week of Jan. 29 through Feb. 2, 2018 would be styled "Earns 0129-0202".
-	2) Analyzes the combined data, restructuring the Earns... table, scoring the
-		metrics used for analysis and restructuring the Earns... table to accommodate
-		the analytical results, all in csv format. (Details to come.)
-	3) Program terminates, and operator uploads it to Google Sheet as input into
-		the trading routine.
+# Earns object contants
+earnsfile = "e.csv"
+earnscolfixes = []
 
-'''
+# Zacks object constants
+zacksfile = "z.csv"
+zackscolfixes = []
+
+# Weeklys object constants
+weeklysfile  = "weeklys.csv"
+weeklyscolfixes = []
+
+# Syms object constants
+ symsfile = "syms.csv"
+ symscolfixes = []
+ 
+class DataBox(datafile, colfixes):
+  # Holds DataFrame and associated attributes and tools.
+   
+  def ___init___():
+    self.Datafile = datafile
+    self.Datafile = datapath + self.Datafile
+    self.Colfixes = colfixes
+    self.Acquire()
+    self.Fixcols()
+  
+  def Acquire():
+  	# Check for the existence of the file, abort if not found
+  	self.Datatable = pd.read_csv(self.Datafile,header=1)
+  	
+  def Fixcols():
+  	if colfixes.empty == True:
+  		pass
+  	else:
+    	self.Datatable.columns = self.Colfixes
+    
+TableEarns = DataBox(earnsfile,earnscolfixes)
+TableZacks = DataBox(zacksfile,zackscolfixes)
+TableWeeklys = Databox(weeklysfile,[])
+Tablesyms = Databox(symsfile,[])
+
+
+
+
 
 def main() :
-	print "In main()"
-
+  print "In main()"
+	
 
 # CALL MAIN
 
