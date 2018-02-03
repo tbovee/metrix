b430c40606a0c4ca531713e719272fa4d0134c0d
tbovee <tbovee@gmail.com>
2018-02-03 12:16:57 -0800

    metrix.py

--- metrix.py
+++ metrix.py
@@ -2,6 +2,9 @@
 
 # Version 0.0
 
+# DTG 20180203.1215 
+#	Removed Syms object as not needed.
+#	Added AnalyzeBox class, structure of the Analyze object
 # DTG 20180203.0700 Added TableMarket object
 # DTG 20180202.1225 Added the data file class and defined the input file objects
   for the 1st run; next step is to define the output file objects.Q: How do I override
@@ -36,14 +39,14 @@ marketcolfixes = []
 weeklysfile  = "weeklys.csv"
 weeklyscolfixes = []
 
-# Syms object constants :: IS THIS REALLY NEEDED??
- symsfile = "syms.csv"
- symscolfixes = []
+# Analyze object constants
+scorecols = []
+filestub = "Earns "
  
 def pausehere()
   # Code that pauses while Market externality is created.
 
-class DataBox(datafile, colfixes):
+class DataBox(datafile, colrename, coldelete):
   # Holds DataFrame and associated attributes and tools.
    
   def ___init___():
@@ -51,27 +54,76 @@ class DataBox(datafile, colfixes):
     self.Datafile = datapath + self.Datafile
     self.Colfixes = colfixes
     self.Acquire()
-    self.Fixcols()
+    self.Colrename()
   
   def Acquire():
   	# Check for the existence of the file, abort if not found
   	self.Datatable = pd.read_csv(self.Datafile,header=1)
   	
-  def Fixcols():
+  def Colrename():
   	if colfixes.empty == True:
-  		pass
+  	  pass
   	else:
-    	self.Datatable.columns = self.Colfixes
+      self.Datatable.columns = self.Colfixes
+    	
+  def Coldelete():
+    if colrename.empty == True:
+      pass
+    else:
+      # Do the deletions
+      pass  
+    	
+
+class AnalyzeBox()
+  # Joins table into a single datastructure, \analyzes
+  # the data, and makes a judgment of each symbol: analyze-dopt and pass-dopt for
+  #   directional optons, analyze-nopt and pass-nopt, for direction neutral optons, 
+  #   and analyze-shr and pass-shr for shares, all of which are long plays.
+  
+  def ___init___:
+    pass
     
+  def joindata():
+    pass
+    
+  def annotations():
+    pass
+    
+  def insertcols(): # if needed for new columns; do in join if possible
+    pass
+    
+  def percentile(col, inverse)
+  	pass
+  	
+  def calcscores()
+    # Sums (or averages) the score columns TBD
+    # For each scorecol:
+    #  percentile(col, inverse) range 0.00 to 1.00
+    #  write percentile to appropriate score column
+    pass
+    
+  def gateclosed(gate):
+    if gate == 1:
+      return 1
+    else:
+      return 0  
+      
+  def options():
+     return [weeklys] #column	  
+	
+  def decision():
+    if gateclosed(gate):
+    	dec = "pass_"options() == 1:
+    	pass
+         
 def main():
 
-  TableEarns = DataBox(earnsfile,earnscolfixes)
-  TableZacks = DataBox(zacksfile,zackscolfixes)
+  TableEarns = DataBox(earnsfile,earnscolfixes,earnscoldeletes)
+  TableZacks = DataBox(zacksfile,zackscolfixes,zackscoldeletes)
   pausehere()
-  TableMarket = DataBox(marketfile,[])
-  TableWeeklys = Databox(weeklysfile,[])
-  TableSyms = Databox(symsfile,[])
-	
+  TableMarket = DataBox(marketfile,[1,"syms"],marketcoldeletes)
+  TableWeeklys = Databox(weeklysfile,[],[])
+  Analyze = AnalyzeBox()
 
 # CALL MAIN
 
