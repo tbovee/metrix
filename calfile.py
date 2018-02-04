59f64d720b78eb2c83d8eeca807c9884697550b2
tbovee <tbovee@gmail.com>
2018-02-04 13:18:17 -0800

    calfile.py

--- calfile.py
+++ calfile.py
@@ -1,37 +1,44 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
 
-# Version 0.0
+# Version 1.0 
 
-# DTG 20180204.0000 Initial coding
+# DTG 20180204.1315 v. 1.0 Working standalone version
+#  Next step: Make it work as an importable module
+# DTG 20180204.xxxx v. 0.0 Initial commit
 
 # Import modules
 import sys
 import os
 
 # Globals
-indir = "./cal"
+DEBUG = 1
+
+indir = "./cal/"
 outfile = "./c.csv"
 splitstr = '"Symbol'
+
 
 def combinefiles(dir,out) :
   files = os.listdir(dir)
   f = 0
   k = 0
   for file in files:
-    Fin = open(file,"r")
-    Fout = open(file,"w")
-    for s = Fin:
+    if DEBUG == 1:
+      print "Processing " + file
+    Fin = open(indir + file,"r")
+    Fout = open(outfile,"w")
+    for s in Fin:
       if f == 0:
         if k == 0:
           linea = s.split(splitstr)
-          Fout.write(splitstr + linea[1])
-          k = k + 1
-          f = f + 1
-        else:
-          if f > 0:
-            k > 0;
-              Fout.print s
-  
+          Fout.write(str(f) + ": " + str(k) + ": " + file + " :: " + splitstr + linea[1])
+      k = k + 1
+      if k > 0:
+        Fout.write(s)
+    Fin.close()
+    f = f + 1
+    k = 0
+  Fout.close()
   
 def main() :
   combinefiles(indir,outfile)
