eecc0228f764f5309ae7ca76ea5fc1db5f7f6872
tbovee <tbovee@gmail.com>
2018-02-04 14:06:09 -0800

    calfile.py

--- calfile.py
+++ calfile.py
@@ -1,17 +1,18 @@
 #! /usr/bin/env python
 
-# Version 1.0 
+# Version 1.01 
 
-# DTG 20180204.1315 v. 1.0 Working standalone version
+# DTG 20180204.1405 v1.01 Fixed an error that zeroed outfile for each infile
+# DTG 20180204.1315 v1.00 Working standalone version
 #  Next step: Make it work as an importable module
-# DTG 20180204.xxxx v. 0.0 Initial commit
+# DTG 20180204.xxxx v0.00 Initial commit
 
 # Import modules
 import sys
 import os
 
 # Globals
-DEBUG = 1
+DEBUG = 0
 
 indir = "./cal/"
 outfile = "./c.csv"
@@ -22,17 +23,16 @@ def combinefiles(dir,out) :
   files = os.listdir(dir)
   f = 0
   k = 0
+  Fout = open(outfile,"w")
   for file in files:
-    if DEBUG == 1:
-      print "Processing " + file
     Fin = open(indir + file,"r")
-    Fout = open(outfile,"w")
     for s in Fin:
       if f == 0:
         if k == 0:
           linea = s.split(splitstr)
           Fout.write(str(f) + ": " + str(k) + ": " + file + " :: " + splitstr + linea[1])
       k = k + 1
+
       if k > 0:
         Fout.write(s)
     Fin.close()
