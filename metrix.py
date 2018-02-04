49bef2b3717d5955f1ac011d2f805416a1deca1e
tbovee <tbovee@gmail.com>
2018-02-03 16:06:39 -0800

    metrix.py

--- metrix.py
+++ metrix.py
@@ -2,13 +2,14 @@
 
 # Version 0.0
 
+# DTG 20180203.1605 Completed field lists except for Analyze object
 # DTG 20180203.1215 
 #	Removed Syms object as not needed.
 #	Added AnalyzeBox class, structure of the Analyze object
 # DTG 20180203.0700 Added TableMarket object
 # DTG 20180202.1225 Added the data file class and defined the input file objects
-  for the 1st run; next step is to define the output file objects.Q: How do I override
-  a method defined in the class?
+#   for the 1st run; next step is to define the output file objects.Q: How do I override
+#   a method defined in the class?
 # DTG 20180129.1950 template created
 # See Documentation.txt for an explanation of metrix.py
 
@@ -23,54 +24,58 @@ import numpy as np
 fsep = "/"
 datapath = "." + fsep + "data" + fsep
 
-# Earns object contants
-calendarfile = "calendar.csv"
-earnscolfixes = []
+# Calendar object contants
+calendarfile = "c.csv"
+earnscolnames = "Symbol" : "Sym"
+earnscoldel = "Company","Market Cap(M)","Estimate","Reported","ESP","PriceChange"
 
 # Zacks object constants
-zacksfile = "zacks.csv"
-zackscolfixes = []
+zacksfile = "z.csv"
+zackscolnames = "Ticker" : "Sym", "Next EPS Rerpot Date" : "Date", "# of Analysts in Q0 Consensus" : "Analysts","Estimate Q0" : "EstQ0","Earnings ESP" : "ESP", "Zacks Rank" : "Rank"
+zackscoldel = "Avg Volume","Market Cap","Optionable","Compare"
 
 # Market object constants
-marketfile = "market.csv"
-marketcolfixes = []
+marketfile = "m.csv"
+marketcolname = "Market Cap" : "Market Cap", "Impl Vol" : "IV","IV_Percentile" : "IVR","FisherTransform" : "FT"
+marketcoldel = []
 
-# Weeklys object constants
+# Weeklys object constants :: Needs column label "Sym", added manually
 weeklysfile  = "weeklys.csv"
-weeklyscolfixes = []
+weeklyscolname = []
+weeklycoldel = []
 
 # Analyze object constants
-scorecols = []
+colscore = []
 filestub = "Earns "
  
 def pausehere()
   # Code that pauses while Market externality is created.
 
-class DataBox(datafile, colrename, coldelete):
+class DataBox(datafile, colname, coldel):
   # Holds DataFrame and associated attributes and tools.
    
   def ___init___():
     self.Datafile = datafile
     self.Datafile = datapath + self.Datafile
-    self.Colfixes = colfixes
     self.Acquire()
-    self.Colrename()
+    self.Delcols()
+    self.Namecols()
   
   def Acquire():
   	# Check for the existence of the file, abort if not found
   	self.Datatable = pd.read_csv(self.Datafile,header=1)
   	
-  def Colrename():
-  	if colfixes.empty == True:
+  def Namecols():
+  	if colname.empty == True:
   	  pass
   	else:
-      self.Datatable.columns = self.Colfixes
+      self.Datatable.rename(index=str, columns={ colname } )
     	
-  def Coldelete():
-    if colrename.empty == True:
+  def Delcols():
+    if colname.empty == True:
       pass
     else:
-      # Do the deletions
+      self.Datatable.drop(columns=[coldel])
       pass  
     	
 
@@ -83,36 +88,36 @@ class AnalyzeBox()
   def ___init___:
     pass
     
-  def joindata():
+  def Joindata():
     pass
     
-  def annotations():
+  def Annotations():
     pass
     
-  def insertcols(): # if needed for new columns; do in join if possible
+  def Insertcols(): # if needed for new columns; do in join if possible
     pass
     
-  def percentile(col, inverse)
+  def Percentile(colscore, inverse)
   	pass
   	
-  def calcscores()
-    # Sums (or averages) the score columns TBD
+  def Calcscores()
+    # Sums (or averages) the colscore columns TBD
     # For each scorecol:
     #  percentile(col, inverse) range 0.00 to 1.00
     #  write percentile to appropriate score column
     pass
     
-  def gateclosed(gate):
+  def Gateclosed(gate):
     if gate == 1:
       return 1
     else:
       return 0  
       
-  def options():
+  def Options():
      return [weeklys] #column	  
 	
-  def decision():
-    if gateclosed(gate):
+  def Decision():
+    if self.Gateclosed(gate):
     	dec = "pass_"options() == 1:
     	pass
          
