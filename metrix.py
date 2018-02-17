b045a88db0323fed7b1e1b089588760e51fb3ba3
tbovee <tbovee@gmail.com>
2018-02-17 12:53:52 -0800

    metrix.py

--- metrix.py
+++ metrix.py
@@ -2,7 +2,8 @@
 
 # Version 0.0
 
-# DTG 20180217.0722 Debugging
+# DTG 20180217.1253 Debugging Class defs and instantiations
+# DTG 20180217.0722 Debugging 
 # DTG 20180215.1845 Splits data saving into proc filetable(); comments out Analysis
 #	for tetng purposes
 # DTG 20180213.1620 Finished def Gateclosed; sketched def Calcscores
@@ -30,17 +31,17 @@ datapath = "." + fsep + "data" + fsep
 
 # Calendar object contants
 calendarfile = "c.csv"
-earnscolnames = '"Symbol" : "Sym"'
-earnscoldel = "Company","Market Cap(M)","Estimate","Reported","ESP","PriceChange"
+calendarcolnames = '"Symbol" : "Sym"'
+calendarcoldel = "Company","Market Cap(M)","Estimate","Reported","ESP","PriceChange"
 
 # Zacks object constants
 zacksfile = "z.csv"
-zackscolnames = '"Ticker" : "Sym", "Next EPS Rerpot Date" : "Date", "# of Analysts in Q0 Consensus" : "Analysts","Estimate Q0" : "EstQ0","Earnings ESP" : "ESP", "Zacks Rank" : "Rank"'
+zackscolnames = {"Ticker" : "Sym", "Next EPS Rerpot Date" : "Date", "# of Analysts in Q0 Consensus" : "Analysts","Estimate Q0" : "EstQ0","Earnings ESP" : "ESP", "Zacks Rank" : "Rank"}
 zackscoldel = "Avg Volume","Market Cap","Optionable","Compare"
 
 # Market object constants
 marketfile = "m.csv"
-marketcolname = '"Market Cap" : "Market Cap", "Impl Vol" : "IV","IV_Percentile" : "IVR","FisherTransform" : "FT"'
+marketcolname = {"Market Cap" : "Market Cap", "Impl Vol" : "IV","IV_Percentile" : "IVR","FisherTransform" : "FT"}
 marketcoldel = []
 
 # Weeklys object constants :: Needs column label "Sym", added manually
@@ -60,42 +61,42 @@ def pausehere():
   # Code that pauses while Market externality is created.
   pass
 
-class DataBox(datafile, colname, coldel):
+class DataBox():
 # Holds DataFrame and associated attributes and tools.
    
-  def ___init___():
+  def __init__(self, datafile, colname, coldel):
     self.Datafile = datafile
     self.Datafile = datapath + self.Datafile
     self.Acquire()
     self.Delcols()
     self.Namecols()
   
-  def Acquire():
+  def Acquire(self):
   	# Check for the existence of the file, abort if not found
   	self.Datatable = pd.read_csv(self.Datafile,header=1)
   	
-  def Namecols():
-  	if colname.empty == True:
+  def Namecols(self):
+    if colname.empty == True:
   	  pass
-  	else:
+    else:
       self.Datatable.rename(index=str, columns={ colname })
       
-  def Delcols():
+  def Delcols(self):
     if colname.empty == True:
       pass
     else:
       self.Datatable.drop(columns=[coldel])
       pass  
     	
-class CombineBox()
-  def ___init___:
+class CombineBox():
+  def __init__(self):
     self.Joindata()
     
-  def Joindata():
+  def Joindata(self):
     self.Interimtable = df.merge(Calendar.Datatable, Zacks.Datatable, how='inner', on='syms')
     self.Datatable = df.merge(self.Interimtable, Weeklys.Datatable, how='outer', on='syms')
         
-  def Insertcols(): # if needed for new columns; do in join if possible
+  def Insertcols(self): # if needed for new columns; do in join if possible
     pass
       	
 '''
@@ -132,15 +133,15 @@ class AnalyzeBox()
     	pass
 '''
 
-def Filetable()
+def Filetable():
 	pd.to_csv(datapath + "out.csv")
          
 def main():
 
-  Calendar = DataBox(earnsfile,earnscolfixes,earnscoldeletes)
-  Zacks = DataBox(zacksfile,zackscolfixes,zackscoldeletes)
+  Calendar = DataBox(calendarfile, calendarcolnames, calendarcoldel)
+  Zacks = DataBox(zacksfile,zackscolnames,zackscoldel)
   pausehere()
-  Market = DataBox(marketfile,[1,"syms"],marketcoldeletes)
+  Market = DataBox(marketfile,[1,"syms"],marketcoldel)
   Weeklys = Databox(weeklysfile,[],[])
   Combine = CombineBox()
 # Analyze = AnalyzeBox()
--- sandbox.py
+++ sandbox.py
@@ -10,22 +10,17 @@ zacksfile = "z.csv"
 # zackscolnames = "Ticker" : "Sym", "Next EPS Rerpot Date" : "Date", "# of Analysts in Q0 Consensus" : "Analysts","Estimate Q0" : "EstQ0","Earnings ESP" : "ESP", "Zacks Rank" : "Rank"
 # zackscoldel = "Avg Volume","Market Cap","Optionable","Compare"
 
+datafile=""
+colname=""
+coldel=""
+
 class DataBox(datafile, colname, coldel):
   # Holds DataFrame and associated attributes and tools.
-   
+
   def ___init___():
     self.Datafile = datafile
     Acquire()
   
-  def Acquire():
-  	# Check for the existence of the file, abort if not found
-  	self.Datatable = pd.read_csv(self.Datafile,header=1)
-  	
-
-def Calcscores():
-    quantiles = 5
-    return Zacks.Datafiles(q=0.5, axis=0)
-
 
 def main():
 
